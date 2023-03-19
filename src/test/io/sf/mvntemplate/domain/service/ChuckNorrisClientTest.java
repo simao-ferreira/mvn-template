d6426a9e62168c09d4480a571ca7e8c14e22e7ab
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-03-19 17:16:59 +0100

    TEMPLATE: Split client from service, update tests

---
+++ src/test/io/sf/mvntemplate/domain/service/ChuckNorrisClientTest.java
@@ -0,0 +1,79 @@
+package io.sf.mvntemplate.domain.service;
+
+import io.sf.mvntemplate.domain.service.exception.ChuckNorrisResponseException;
+import io.sf.mvntemplate.domain.service.model.JokeDTO;
+import lombok.SneakyThrows;
+import okhttp3.ResponseBody;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import retrofit2.Response;
+import retrofit2.mock.Calls;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class ChuckNorrisClientTest {
+
+    @InjectMocks
+    private ChuckNorrisClient client;
+
+    @Mock
+    private ChuckNorrisConnector connector;
+
+    @Test
+    @SneakyThrows
+    void whenJokeIsRequested_shouldReturnSuccessfulResponse() {
+        //given
+        JokeDTO joke = JokeDTO.builder()
+                .value("Chuck Norris can delete the Recycling Bin")
+                .build();
+        Response<JokeDTO> successfulJoke = Response.success(joke);
+        when(connector.getJoke()).thenReturn(Calls.response(successfulJoke));
+        //when
+        JokeDTO jokeResponse = client.getChuckNorrisJoke();
+        //then
+        assertNotNull(jokeResponse);
+        assertEquals(joke.getValue(), jokeResponse.getValue());
+    }
+
+    @Test
+    @SneakyThrows
+    void whenJokeIsRequested_andResponseBodyIsNull_shouldThrowChuckNorrisException() {
+        //given
+        Response<JokeDTO> nullJoke = Response.success(null);
+        when(connector.getJoke()).thenReturn(Calls.response(nullJoke));
+        //when
+        Exception exception = assertThrows(
+                ChuckNorrisResponseException.class,
+                () -> client.getChuckNorrisJoke()
+        );
+        //then
+        assertEquals(
+                "Failed to get joke with Response{protocol=http/1.1, code=200, message=OK, url=http://localhost/}",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    @SneakyThrows
+    void whenJokeIsRequested_andResponseIsNotSuccessful_shouldThrowChuckNorrisException() {
+        //given
+        ResponseBody errorResponse = ResponseBody.create("", null);
+        Response<JokeDTO> successfulJoke = Response.error(404, errorResponse);
+        when(connector.getJoke()).thenReturn(Calls.response(successfulJoke));
+        //when
+        Exception exception = assertThrows(
+                ChuckNorrisResponseException.class,
+                () -> client.getChuckNorrisJoke()
+        );
+        //then
+        assertEquals(
+                "Failed to get joke with Response{protocol=http/1.1, code=404, message=Response.error(), url=http://localhost/}",
+                exception.getMessage()
+        );
+    }
+
+}
